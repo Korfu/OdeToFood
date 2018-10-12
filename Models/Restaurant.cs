6f122eb0b5e45c3b029a2975e81995cc87435d48
unknown <konrad.korf@3xr.local>
2018-10-12 14:00:14 +0200

    EF core impemented

---
+++ Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(80)]
+        [Display(Name="Restaurant Name")]
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ ViewModels/RestaurantEditModel.cs
@@ -0,0 +1,16 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewModels
+{
+    public class RestaurantEditModel
+    {
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
