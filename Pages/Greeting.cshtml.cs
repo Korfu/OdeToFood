234687c64ff1210924997d6640ca8964031d618f
unknown <konrad.korf@3xr.local>
2018-10-19 15:51:28 +0200

    partial view added

--- Pages/Greeting.cshtml.cs
+++ Pages/Greeting.cshtml.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
 
 namespace OdeToFood.Pages
 {
     public class GreetingModel : PageModel
     {
-        public void OnGet()
+        private IGreeter _greeter;
+        public string CurrentGreeting { get; set; }
+
+        public GreetingModel(IGreeter greeter)
         {
+            _greeter = greeter;
+        }
+        public void OnGet(string name)
+        {
+            CurrentGreeting = $"{name}: { _greeter.GetMessageOfTheDay()}";
         }
     }
 }
